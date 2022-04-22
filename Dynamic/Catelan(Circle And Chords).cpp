04fbec0d6b9a527531bf7b706f04e7a9e955eeed
Ajit Kumar <60688738+AJITKUMAR130012@users.noreply.github.com>
2022-04-22 13:35:53 +0530

    Create Catelan(Circle And Chords).cpp

---
+++ Dynamic/Catelan(Circle And Chords).cpp
@@ -0,0 +1,34 @@
+Problem Description: https://www.pepcoding.com/resources/data-structures-and-algorithms-in-java-levelup/dynamic-programming/circle-and-chords-official/ojquestion
+Time Complexity: o(n)
+Space Complexity: o(n)
+
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int catelan(int n)
+{
+    vector<int> t(n+1,0);
+    t[0]=1;
+    t[1]=1;
+    for(int i=2;i<=n;i++)
+    {
+        for(int j=0;j<i;j++)
+        {
+            int x=t[j]*t[i-j-1];
+            t[i]+=x;
+        }
+    }
+    return t[n];
+}
+// if input is n then we have total 2*n point on the circle
+// need to count how many non-intersecting chord can be drawn
+// it is eqaul to the catelan number
+
+int main()
+{
+    int n;
+    cin>>n;
+    cout<<catelan(n);
+    return 0;
+}
