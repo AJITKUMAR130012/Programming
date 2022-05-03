c99230aefbcf64c6c2aa02fa13fa9bf016caf678
Ajit Kumar <60688738+AJITKUMAR130012@users.noreply.github.com>
2022-05-03 19:23:46 +0530

    Create Immediate Smaller Element.cpp

---
+++ Stack/Immediate Smaller Element.cpp
@@ -0,0 +1,17 @@
+Time Complexity: o(n)
+Space COmplexity: o(1)
+  
+  	void immediateSmaller(int *arr, int n) {
+	    //  code here
+	  for(int i=0;i<n-1;i++)
+	  {
+	      if(arr[i]>arr[i+1])
+	      {
+	          arr[i]=arr[i+1];
+	      }
+	      else arr[i]=-1;
+	  }
+	  arr[n-1]=-1;
+	   
+	   
+	}
