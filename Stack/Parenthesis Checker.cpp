65b15cceb6b6b5167e93955427de1bf99d1c0320
Ajit Kumar <60688738+AJITKUMAR130012@users.noreply.github.com>
2022-05-05 09:56:20 +0530

    Create Parenthesis Checker.cpp

---
+++ Stack/Parenthesis Checker.cpp
@@ -0,0 +1,29 @@
+Problem statement: https://practice.geeksforgeeks.org/problems/parenthesis-checker2744/1/?page=1&difficulty[]=0&status[]=unsolved&category[]=Stack&sortBy=submissions#
+Time Complexity: o(n)
+Space Complexity: o(n)  --> size of stack
+
+ bool ispar(string x)
+    {
+        // Your code here
+        if(x.length()&1) return false;
+        stack<char> s;
+        for(int i=0;i<x.length();i++)
+        {
+            if(x[i]=='(' or x[i]=='{' or x[i]=='[')
+            {
+                if(x[i]=='(')
+                s.push(')');
+                else if(x[i]=='{') s.push('}');
+                else s.push(']');
+             //   cout<<"1"<<endl;
+            }
+            else
+            {
+                if(s.size()==0) return false;
+                if(x[i]!=s.top()) return false;
+                else s.pop();
+              //  cout<<"2"<<endl;
+            }
+        }
+        return true;
+    }
