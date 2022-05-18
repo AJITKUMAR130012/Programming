16124229ebd9699a7aca07740850c333c04e6444
Ajit Kumar <60688738+AJITKUMAR130012@users.noreply.github.com>
2022-05-18 08:49:00 +0530

    Create Height of Heap.cpp

---
+++ Heap/Height of Heap.cpp
@@ -0,0 +1,9 @@
+Problem Description: https://practice.geeksforgeeks.org/problems/is-binary-tree-heap/1/?page=1&difficulty[]=0&category[]=Heap&sortBy=submissions#
+Time Complexity: o(1)
+Space Complexity: o(1)
+  
+  
+   int heapHeight(int N, int arr[]){
+        // code here
+        return (log(N)/log(2.0));
+    }
